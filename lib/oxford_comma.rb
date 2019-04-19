c172719b1a9e74f72b6d2ae3c3da090ba2861494
Oscar Shoudt <shoudto@gmail.com>
2019-04-19 18:09:07 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
-end
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  elsif array.length == 3
+    "#{array[0]}, #{array[1]}, and #{array[2]}"
+  else 
+    array[-1] = "and " + array[-1]
+    array.join(", ")
+  end
+end 
+  
